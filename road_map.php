56783bf128ad5f3ddd4429a30fd3922bdfe47a49
denislav <58664415+denislavmarinov@users.noreply.github.com>
2020-02-05 17:41:54 +0200

    road_map file update
    Update the road map file. The file output is the same as the expected.

--- road_map.php
+++ road_map.php
@@ -3,5 +3,23 @@ session_name('traffic_lights');
 session_start();
 
 $roadMap = $_SESSION['roadMap'];
+$seconds = 0;
+$stoplight_flash = 0;
+$distance = 0;
 
-var_dump($roadMap);
+for ($i=1; $i <= count($roadMap); $i++) { 
+
+	$seconds = $seconds + $roadMap[$i][0] - $distance;
+
+	$distance = (int)$roadMap[$i][0];
+
+	$stoplight_flash = (int)$roadMap[$i][1];
+
+	if(($seconds / $stoplight_flash) % 2 != 0){
+		$seconds = (int)($seconds / $stoplight_flash + 1) * $stoplight_flash;
+	}
+	else{
+		$seconds = (int)$seconds;
+	}
+}
+echo "The seconds that you will need to pass all traffic lights is:   <b>" . $seconds . "</b>";
