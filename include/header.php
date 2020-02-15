610256d329ebdc20669713723add81ff85d2ca44
denislav <58664415+denislavmarinov@users.noreply.github.com>
2020-02-15 14:47:31 +0200

    Create header includes
    Create header includes

--- include/header.php
+++ include/header.php
@@ -1,1 +1,13 @@
-
+<?php
+session_name('traffic_lights');
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TrafficLights1D</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+</head>
+<body>
